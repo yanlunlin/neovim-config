c933c03bba1dcbb875d02c788509984756c934dc
yanlunlin <yenlunlin1008@gmail.com>
2026-05-15 00:05:44 +0800

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,10 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+build = function()
+  require("blink.cmp").build():wait(60000)
+end
+
 -- Setup lazy.nvim
 require("lazy").setup({
   spec = {
